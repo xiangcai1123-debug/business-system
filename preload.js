9a0124523fd92c59bc7cfc154c900c40a4a6010c
xiangcai1123-debug <xiang.cai1123@gmail.com>
2026-03-29 19:22:40 +0800

    Update preload.js

---
+++ preload.js
@@ -0,0 +1,44 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// 暴露 API 给渲染进程
+contextBridge.exposeInMainWorld('electronAPI', {
+  // 客户管理
+  getCustomers: () => ipcRenderer.invoke('get-customers'),
+  addCustomer: (data) => ipcRenderer.invoke('add-customer', data),
+  
+  // 商机管理
+  getOpportunities: () => ipcRenderer.invoke('get-opportunities'),
+  addOpportunity: (data) => ipcRenderer.invoke('add-opportunity', data),
+  
+  // 询报价
+  getQuotations: () => ipcRenderer.invoke('get-quotations'),
+  addQuotation: (data) => ipcRenderer.invoke('add-quotation', data),
+  
+  // 订单管理
+  getOrders: () => ipcRenderer.invoke('get-orders'),
+  addOrder: (data) => ipcRenderer.invoke('add-order', data),
+  
+  // 跟单管理
+  getFollowups: () => ipcRenderer.invoke('get-followups'),
+  addFollowup: (data) => ipcRenderer.invoke('add-followup', data),
+  
+  // 发货单证
+  getShipments: () => ipcRenderer.invoke('get-shipments'),
+  addShipment: (data) => ipcRenderer.invoke('add-shipment', data),
+  
+  // 资金管理
+  getTransactions: () => ipcRenderer.invoke('get-transactions'),
+  addTransaction: (data) => ipcRenderer.invoke('add-transaction', data),
+  
+  // 供应商管理
+  getSuppliers: () => ipcRenderer.invoke('get-suppliers'),
+  addSupplier: (data) => ipcRenderer.invoke('add-supplier', data),
+  
+  // 采购管理
+  getPurchases: () => ipcRenderer.invoke('get-purchases'),
+  addPurchase: (data) => ipcRenderer.invoke('add-purchase', data),
+  
+  // 模板库
+  getTemplates: () => ipcRenderer.invoke('get-templates'),
+  addTemplate: (data) => ipcRenderer.invoke('add-template', data)
+});
